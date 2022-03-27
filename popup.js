19dee23e60ab1d86216ad26b37411e6eea1f218d
brytebee <brytebee@gmail.com>
2022-03-27 23:35:47 +0100

    :fire: Cleanup
    Cleaned up unused functions

--- popup.js
+++ popup.js
@@ -2,36 +2,16 @@
 let changeColor = document.getElementById("changeColor");
 
 changeColor.addEventListener("click", () => {
-  chrome.tabs.query(
-    { active: true, currentWindow: true },
-    function (arrayOfTabs) {
-      chrome.tabs.reload(arrayOfTabs[0].id);
-    }
+  setInterval(
+    chrome.tabs.query(
+      { active: true, currentWindow: true },
+      function (arrayOfTabs) {
+        chrome.tabs.reload(arrayOfTabs[0].id);
+      }
+    ),
+    4000
   );
 });
-
-// chrome.storage.sync.get("color", ({ color }) => {
-//   changeColor.style.backgroundColor = color;
-// });
-
-// // When the button is clicked, inject setPageBackgroundColor into current page
-// // changeColor.addEventListener("click", async () => {
-// //   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-// //   chrome.scripting.executeScript({
-// //     target: { tabId: tab.id },
-// //     function: location.reload();
-// //   });
-// // });
-
-// changeColor.addEventListener("click", async () => {
-//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-//   chrome.tabs.getSelected(null, function (tab) {
-//     var code = "window.location.reload();";
-//     chrome.tabs.executeScript(tab.id, { code: code });
-//   });
-// });
 
 // // The body of this function will be executed as a content script inside the
 // // current page
