9f36be529ccbd3f07fb1bf18ac05283c80dc202d
brytebee <brytebee@gmail.com>
2022-03-27 23:46:56 +0100

    :white_check_mark: Add setInterval
    Added setInterval to keep reload going

--- popup.js
+++ popup.js
@@ -2,19 +2,18 @@
 let changeColor = document.getElementById("changeColor");
 
 changeColor.addEventListener("click", () => {
-  setInterval(
+  setInterval(() => {
     chrome.tabs.query(
       { active: true, currentWindow: true },
       function (arrayOfTabs) {
         chrome.tabs.reload(arrayOfTabs[0].id);
       }
-    ),
-    4000
-  );
+    );
+  }, 5000);
 });
 
-// // The body of this function will be executed as a content script inside the
-// // current page
+// The body of this function will be executed as a content script inside the
+// current page
 // function setPageBackgroundColor() {
 //   chrome.storage.sync.get("color", ({ color }) => {
 //     document.body.style.backgroundColor = color;
