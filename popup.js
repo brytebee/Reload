68923c4dc91cb1a4eca13ef0fc06339d486d36f9
brytebee <brytebee@gmail.com>
2022-03-28 23:49:39 +0100

    :white_check_mark: Add mutation observer
    Added mutation observer to monitor changes in the DOM

--- popup.js
+++ popup.js
@@ -1,16 +1,48 @@
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+let changeColor = document.getElementById('changeColor');
 
-changeColor.addEventListener("click", () => {
-  setInterval(() => {
-    chrome.tabs.query(
-      { active: true, currentWindow: true },
-      function (arrayOfTabs) {
-        chrome.tabs.reload(arrayOfTabs[0].id);
+const reviewProject = document.querySelector('.review-request-project-name');
+const noReviews = document.querySelector('.review-request-announcement-header');
+const reviewContainer = document.querySelector('.list-unstyled');
+
+const refresh = () => {
+  changeColor.addEventListener('click', () => {
+    setTimeout(() => {
+      chrome.tabs.query(
+        { active: true, currentWindow: true },
+        function (arrayOfTabs) {
+          chrome.tabs.reload(arrayOfTabs[0].id);
+        }
+      );
+    }, 500);
+    setInterval(() => {
+      chrome.tabs.query(
+        { active: true, currentWindow: true },
+        function (arrayOfTabs) {
+          chrome.tabs.reload(arrayOfTabs[0].id);
+        }
+      );
+    }, 2000);
+  });
+};
+
+let observer = new MutationObserver((mutations) => {
+  refresh();
+  for (let mutation of mutations) {
+    for (let addedNode of mutation.addedNodes) {
+      if (addedNode.nodeName === 'INPUT[CHECKBOX]') {
+        console.log('YES', addedNode);
       }
-    );
-  }, 5000);
+    }
+  }
 });
+
+observer.observe(document, { childList: true, subtree: true });
+
+// function nodeInsertedCallback(event) {
+//   console.log(event);
+// }
+// document.addEventListener("DOMNodeInserted", nodeInsertedCallback);
 
 // The body of this function will be executed as a content script inside the
 // current page
